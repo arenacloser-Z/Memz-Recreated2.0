4086038a0cf731bdc7bed86afc32b8dd9c088721
arenacloser-Z <166559903+arenacloser-Z@users.noreply.github.com>
2024-05-08 12:34:52 -0400

    Update listener.py

--- listener.py
+++ listener.py
@@ -1,1 +1,3 @@
-
+// this code will add a listener that makes sure the victim(even though this code is harmless) does not attempt to end the tast.
+// if this happens, the code will launch a barrage of errors that will result in a BSOD
+// this will likely be created into a seperate, larger project
